351bc6f9b86f0648299616180cc928992f6e155f
Markiesch <marksmeel@gmail.com>
2025-06-22 19:02:57 +0200

    #23 automatische verkoop overzicht

--- resources/js/pages/admin/SalesSummaries.vue
+++ resources/js/pages/admin/SalesSummaries.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { Table, TableBody, TableCell, TableEmpty, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Head, usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
 import { route } from 'ziggy-js';
+import { useFormat } from '@/composables/useFormat';
 
 const breadcrumbs = [
     {
@@ -12,10 +13,20 @@ const breadcrumbs = [
     },
 ];
 
+const { formatDate } = useFormat();
+
 const files = computed(() => usePage().props.files || []);
 
 function basename(path: string) {
     return path.split(/[\\/]/).pop();
+}
+
+function getDateFromFilename(file: string) {
+    const match = file.match(/sales_summary_(\d{4})_(\d{2})_(\d{2})/);
+    if (match) {
+        return formatDate(`${match[1]}-${match[2]}-${match[3]}`);
+    }
+    return '-';
 }
 </script>
 
@@ -23,10 +34,10 @@ function basename(path: string) {
     <AppLayout :breadcrumbs="breadcrumbs">
         <Head title="Dagelijkse Verkoop Samenvattingen" />
         <div class="container mt-4">
-            <h1 class="text-xl font-bold pb-1">Dagelijkse Verkoop Samenvattingen</h1>
+            <h1 class="pb-1 text-xl font-bold">Dagelijkse Verkoop Samenvattingen</h1>
             <p class="text-base">Download hier de automatisch gegenereerde Excel-bestanden met de omzet per dag.</p>
 
-            <div class="rounded border mt-4">
+            <div class="mt-4 rounded border">
                 <Table>
                     <TableHeader>
                         <TableRow>
@@ -37,7 +48,7 @@ function basename(path: string) {
                     </TableHeader>
                     <TableBody>
                         <TableRow v-for="file in files" :key="file">
-                            <TableCell class="col-span-2">{{ file }}</TableCell>
+                            <TableCell>{{ getDateFromFilename(file) }}</TableCell>
                             <TableCell>{{ basename(file) }}</TableCell>
                             <TableCell>
                                 <a :href="route('admin.sales_summaries.download', { filename: basename(file) })" class="btn btn-sm btn-primary"
